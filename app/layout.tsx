c69bebf49b5eddd7019db222ace6261f1539abd4
dot-1x <mugiwaraluffy2003@gmail.com>
2026-02-19 22:57:33 +0700

    feat: added opengraph for porto

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,31 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "dotcchix || Zex",
   description:
-    'Portofolio websites about Zex. "Assisted" with Claude Opus 4.6 with a little touch from me',
+    'Zexs personal websites, where you can find about me here. "Assisted" with Claude Opus 4.6 with a little touch from me',
+
+  openGraph: {
+    title: "dotcchix || Zex.",
+    description:
+      'Zexs personal websites, where you can find about me here. "Assisted" with Claude Opus 4.6 with a little touch from me',
+    images: [
+      {
+        url: "https://cdn.discordapp.com/avatars/732842920889286687/7a3da245a49f565d5d252935f0eec388.webp",
+        width: 128,
+        height: 128,
+        alt: "dotcchix || Zex.",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary",
+    title: "dotcchix || Zex.",
+    description:
+      'Zexs personal websites, where you can find about me here. "Assisted" with Claude Opus 4.6 with a little touch from me',
+    images: [
+      "https://cdn.discordapp.com/avatars/732842920889286687/7a3da245a49f565d5d252935f0eec388.webp",
+    ],
+  },
 }
 
 export default function RootLayout({
